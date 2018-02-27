23af4e52960449bf95d1a0a34198794a44867133
Jonathan Fejtek <jonathan.fejtek@mail.utoronto.ca>
2018-02-26 23:44:35 -0500

    removed unused dependency in sequence-track

--- dev/scripts/sequence-track.js
+++ dev/scripts/sequence-track.js
@@ -2,20 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SequenceCell from './sequence-cell';
 
-function arraysEqual(a, b) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length != b.length) return false;
-  
-    // If you don't care about the order of the elements inside
-    // the array, you should sort both arrays here.
-  
-    for (var i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
-    }
-    return true;
-  }
-
 /**
  * A SequenceTrack Component. Comprises an array of SequenceCells.
  */
